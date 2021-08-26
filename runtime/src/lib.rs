199b6736f2806b180a7173a98b31ef0864ba26c1
Martin <martin.hloska@gmail.com>
2021-08-26 14:18:38 +0200

    add electiosn to benchmarks

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1114,6 +1114,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, pallet_balances, Balances);
 			add_benchmark!(params, batches, pallet_timestamp, Timestamp);
 			add_benchmark!(params, batches, pallet_democracy, Democracy);
+			add_benchmark!(params, batches, pallet_elections_phragmen, Elections);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
